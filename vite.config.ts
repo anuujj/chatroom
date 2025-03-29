5238db242c22e6d75c77c982f01b455705bb9e3b
Anuj <anujS@Apples-MacBook-Pro.local>
2025-03-29 15:50:05 +0530

    updated workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/chatroom/', 
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html',
+    }
+  }
 })
